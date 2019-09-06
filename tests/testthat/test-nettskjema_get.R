daf6e6905ee162bf45f63fcb732227d22f1ed6e0
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2019-09-06 11:21:06 +0200

    testing if tests are making CI fail

--- tests/testthat/test-nettskjema_get.R
+++ tests/testthat/test-nettskjema_get.R
@@ -1,17 +1,17 @@
-# Set test params
-test_token <- "NETTSKJEMA_TEST_TOKEN"
-test_form <- 123823
-
-test_that("nettskjema_get_data works", {
-  skip_if_no_auth()
-  test_dt <- nettskjema_get_data(test_form, token_name = test_token)
-
-  expect_equal(class(test_dt), c("tbl_df", "tbl", "data.frame"))
-})
-
-# test_that("nettskjema_get_meta works",{
+# # Set test params
+# test_token <- "NETTSKJEMA_TEST_TOKEN"
+# test_form <- 123823
 #
-#   meta <- nettskjema_get_meta(form_id = test_form,
-#                               token_name = test_token)
+# test_that("nettskjema_get_data works", {
+#   skip_if_no_auth()
+#   test_dt <- nettskjema_get_data(test_form, token_name = test_token)
 #
+#   expect_equal(class(test_dt), c("tbl_df", "tbl", "data.frame"))
 # })
+#
+# # test_that("nettskjema_get_meta works",{
+# #
+# #   meta <- nettskjema_get_meta(form_id = test_form,
+# #                               token_name = test_token)
+# #
+# # })
