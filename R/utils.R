adbe3851e593f6aa9ba9917ec4b59c7189cc4c32
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-10-15 10:37:09 +0200

    move api funcs to own file

--- R/utils.R
+++ R/utils.R
@@ -13,53 +13,6 @@ max_selected <- function(x){
   ifelse(t == 0, NaN, t)
 }
 
-is.response <- function(x) class(x) == "response"
-
-is.app_json <- function(x){
-  if(!is.response(x)) return(FALSE)
-  http_type(x) == "application/json"
-}
-
-#' @importFrom httr http_type content http_error status_code
-#' @importFrom jsonlite fromJSON
-api_catch_error <- function(resp){
-  if (!is.app_json(resp)) {
-    stop("API did not return json", call. = FALSE)
-  }
-
-  parsed <- fromJSON(content(resp, "text"),
-                     simplifyVector = FALSE)
-
-  if (http_error(resp)) {
-    stop(
-      sprintf(
-        "Nettskjema API request failed with error\n %s : %s\n",
-        status_code(resp), parsed$message),
-      call. = FALSE
-    )
-  }
-}
-
-
-#' @importFrom httr http_type content http_status
-#' @importFrom jsonlite fromJSON
-api_catch_empty <- function(resp){
-  if (http_type(resp) != "application/json") {
-    stop("API did not return json", call. = FALSE)
-  }
-
-  parsed <- fromJSON(content(resp, "text"), simplifyVector = FALSE)
-
-  if (resp$status_code == 500) {
-    stop(
-      sprintf(
-        "Nettskjema API request with \n %s",
-        http_status(resp)$message),
-      call. = FALSE
-    )
-  }
-}
-
 check_element <- function(x){
   if(length(x) == 1)
     return(x)
