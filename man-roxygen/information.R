f4dafb5b18c782ded97535887ebeb903b793a556
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-10-30 23:45:12 +0200

    fix spelling

--- man-roxygen/information.R
+++ man-roxygen/information.R
@@ -1,4 +1,4 @@
 #' @param information character or list vector of additional answer information
 #' to add (One or more of "order", "option", "correct" "preselected"). If the list
 #' is named, the additional information suffixes will be changed to the list names. If NULL (default) nothing is added.
-#' Currently not combinable with \code{use_codebook = TRUE}.
+#' Currently not possible to combine with \code{use_codebook = TRUE}.
