d78f954179d04d70fd6f89357617bafb85ca0f36
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-03-18 11:52:24 +0100

    use rappdirs for user token cache

--- R/ns-auth.R
+++ R/ns-auth.R
@@ -39,13 +39,13 @@ ns_req <- function(...) {
 #'  this is stored in .Renviron as "ns_CLIENT_SECRET"
 #' @param cache Logical. Should the token be cached?
 #' @param cache_path Character. File path to where
-#'   the token should be stored. Defaults to user
-#'   home directory
+#'   the token should be stored. Defaults to system
+#'   cache directory.
 ns_auth_token <- function(
   client_id = Sys.getenv("NETTSKJEMA_CLIENT_ID"),
   client_secret = Sys.getenv("NETTSKJEMA_CLIENT_SECRET"),
   cache = TRUE,
-  cache_path = fs::path_home(".nettskjema_token.rds")
+  cache_path = NULL
 ) {
   req <- httr2::request(
     "https://authorization.nettskjema.no/oauth2/token"
@@ -61,6 +61,11 @@ ns_auth_token <- function(
     )
 
   if (cache) {
+    if (is.null(cache_path))
+      cache_path <- file.path(
+        rappdirs::user_cache_dir(),
+        ".nettskjema-token.rds"
+      )
     req <- req |>
       httr2::req_cache(
         cache_path,
